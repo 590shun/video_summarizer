40ff2f5bf78717245a2335643d163c87649e74ae
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2021-09-20 23:18:39 +0900

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -8,22 +8,21 @@ from utils.eval import evaluate_scores
 
 # Evaluation (Kendall's & Spearman's correlation)
 # setting
-m = h5py.File('./logs/timestamp_TransformerTrainer/dataset_splits.json_preds.h5', 'r')
+m = h5py.File('./logs/1632145246_DSNTrainer/summe_splits.json_preds.h5', 'r')
+u = h5py.File('./datasets/summarizer_dataset_summe_google_pool5.h5', 'r')
 
-
-u = h5py.File('./datasets/summarizer_dataset.h5', 'r')
 # load video list
-path_text = './video_list.txt'
+path_text = './summe_video_list.txt'
 with open(path_text) as f:
     video_list = f.read().splitlines()
 
-i = 1
 kendall_list = []
 spearman_list = []
 
-for video in video_list:
+for i in range(len(video_list)):
+    i += 1
     usr = u['video_' + str(i)]['user_scores'][()]
-    machine = m['summarizer_dataset_as.h5']['video_' + str(i)]['machine_scores'][()]
+    machine = m['summarizer_dataset_summe_google_pool5.h5']['video_' + str(i)]['machine_scores'][()]
     spe = evaluate_scores(machine, usr, metric="spearmanr")
     ken = evaluate_scores(machine, usr, metric="kendalltau")
     spearman_list.append(spe)
